4c2e3b03b6924e899443eb3ea482f9ead7b0c1ad
geomarian <geomarian520@gmail.com>
2022-09-07 10:00:59 +0530

    Added UC3 Employee Fulltime and Parttime salary

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -11,20 +11,35 @@ import java.util.Random;
  */
 public class EmployeeWage {
 
-	
-		public static void main(String[] args) {
-			// Driving Code
-			Random rand = new Random();
-			System.out.println("Employee attendance is: ");
-			int k = rand.nextInt(2);
-			System.out.println(k);
-			if(k==0) {
-				System.out.println("Employee is Absent: " + k);
-			}
-				else if(k==1){
-					System.out.println("Employee is Present: " + k );
-				}
-			}
+	/**
+	 * @param args
+	 */
+	//UC3
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+		Random rand = new Random();
+		System.out.println("Employee attendance is= ");
+		int k = rand.nextInt(3);
+		System.out.println(k);
+
+		if(k==0) {
+			System.out.println("Employee is Absent ");
+			System.out.println("No salary because employee is absent");
+		}
+		else if(k==1){
+			System.out.println("Employee is Full Time Present ");
+			int empRatePerHr=20; 
+			int	empHrs=8;
+			int salary = empRatePerHr*empHrs;
+			System.out.println(salary);
+		}
+		else if(k==2) {
+			System.out.println("Employee is Part Time Present");
+			int empRatePerHr=20; 
+			int	empHrs=4;
+			int salary = empRatePerHr*empHrs;
+			System.out.println(salary);
 
 }
+	}}
