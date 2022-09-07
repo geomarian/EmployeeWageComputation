54a543b3000f0fc2d4adb0bad60f1fc1e5fb52b0
geomarian <geomarian520@gmail.com>
2022-09-07 10:22:58 +0530

    Edited and Updated UC2

---
+++ src/com/bridgelabz/EmployeeWageUC1.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+import java.util.Random;
+
+/**
+ * @author ASUS
+ *
+ */
+public class EmployeeWageUC1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rand = new Random();
+		System.out.println("Employee attendance is: ");
+		int k = rand.nextInt(2);
+		System.out.println(k);
+	
+	if(k==0) {
+		System.out.println("Employee is Absent ");
+		System.out.println("No salary because employee is absent");
+	}
+		else if(k==1){
+			System.out.println("Employee is Present ");
+			int empRatePerHr=20; 
+			int	empHrs=8;
+			int salary = empRatePerHr*empHrs;
+			System.out.println(salary);
+		}
+ 
+	
+	
+	}
+
+
+
+	}
+
+
