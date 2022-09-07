58e38d4af9a313a3116d3c926beeac00fdaa0331
geomarian <geomarian520@gmail.com>
2022-09-07 10:05:04 +0530

    Edited & updated UC2

--- src/com/bridgelabz/UC1_EmployeeWage.java
+++ src/com/bridgelabz/UC1_EmployeeWage.java
@@ -32,49 +32,27 @@ public class UC1_EmployeeWage {
 //			}
 //		}
 //
-//	UC -2
-//		Random rand = new Random();
-//		System.out.println("Employee attendance is: ");
-//		int k = rand.nextInt(2);
-//		System.out.println(k);
-//	
-//	if(k==0) {
-//		System.out.println("Employee is Absent ");
-//		System.out.println("No salary because employee is absent");
-//	}
-//		else if(k==1){
-//			System.out.println("Employee is Present ");
-//			int empRatePerHr=20; 
-//			int	empHrs=8;
-//			int salary = empRatePerHr*empHrs;
-//			System.out.println(salary);
-//		}
- 
-		// UC- 3
+	
 		Random rand = new Random();
-		System.out.println("Employee attendance is= ");
-		int k = rand.nextInt(3);
+		System.out.println("Employee attendance is: ");
+		int k = rand.nextInt(2);
 		System.out.println(k);
-
-		if(k==0) {
-			System.out.println("Employee is Absent ");
-			System.out.println("No salary because employee is absent");
-		}
+	
+	if(k==0) {
+		System.out.println("Employee is Absent ");
+		System.out.println("No salary because employee is absent");
+	}
 		else if(k==1){
-			System.out.println("Employee is Full Time Present ");
+			System.out.println("Employee is Present ");
 			int empRatePerHr=20; 
 			int	empHrs=8;
 			int salary = empRatePerHr*empHrs;
 			System.out.println(salary);
 		}
-		else if(k==2) {
-			System.out.println("Employee is Part Time Present");
-			int empRatePerHr=20; 
-			int	empHrs=4;
-			int salary = empRatePerHr*empHrs;
-			System.out.println(salary);
+ 
+	
 	
 	}
 
 }
-}
+
